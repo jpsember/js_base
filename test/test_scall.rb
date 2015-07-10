7decba41ac3e59355a95c9ef9d033141de5a40a7
Jeff Sember <jpsember@gmail.com>
2015-07-10 15:06:18 -0700

    Issue #2: added unit test demonstrating the problem

---
+++ test/test_scall.rb
@@ -0,0 +1,10 @@
+require 'js_base/js_test'
+
+class TestSCall < JSTest
+
+  def test_scall_capture_call_nonexistent_program
+    result,success = scall('fooxyz arg1 arg2', false)
+    assert_false(success)
+  end
+
+end
