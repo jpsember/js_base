1be460efb41862f670d0c68d5f7ccc922a7dcf73
Jeff Sember <jpsember@gmail.com>
2015-10-14 23:04:02 -0700

    added store_or_delete Hash method

--- lib/js_base/hash_utils.rb
+++ lib/js_base/hash_utils.rb
@@ -10,6 +10,17 @@ class Hash
       replace(reverse_merge(other_hash))
     end
 
+
+  end
+
+  # Store a key/value, or remove key if new value is nil
+  #
+  def store_or_delete(key,value_or_nil)
+    if value_or_nil
+      store(key,value_or_nil)
+    else
+      delete(key)
+    end
   end
 
 end
